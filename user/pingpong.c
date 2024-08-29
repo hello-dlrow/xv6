850634492076b0447e281b1f8b58c62e8f2e4251
baba <baba@MacBook-Air.local>
2024-08-28 23:54:10 -0700

    update for prime.c

--- user/pingpong.c
+++ user/pingpong.c
@@ -1,14 +1,34 @@
-nt p[2];
-  char buf[100];
-  pipe(p);
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
 
-  int pid = fork();
-  if (pid == 0) {
-    write(p[1], “ping”, 4);
-    printf(“Thread id %d: received ping\n”, getpid());
-  } 
-  else {
-    wait(0);
-    read(p[0], buf, 4);
-    printf(“Thread id %d: received pong\n”, getpid());
-  }
+int
+main(int argc, char *argv[])
+{
+	int p[2];  // file descriptors for pipe
+	char recv_buf[128];
+
+	pipe(p);
+
+	if (fork() == 0) {  // child
+		read(p[0], recv_buf, 4);
+		close(p[0]);
+
+		printf("%d: received %s\n", getpid(), recv_buf);
+
+		write(p[1], "pong", 4);
+		close(p[1]);
+
+		exit(0);
+	} else {  // parent
+		write(p[1], "ping", 4);
+		close(p[1]);
+
+		read(p[0], recv_buf, 4);
+		close(p[0]);
+
+		printf("%d: received %s\n", getpid(), recv_buf);
+
+		exit(0);
+	}
+}
