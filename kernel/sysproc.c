6e53199bdfb1d68ab1e10a8abdaf3910625c4bc1
baba <baba@MacBook-Air.local>
2024-08-29 21:58:40 -0700

    update for prime.c

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -120,7 +120,7 @@
 
     si.freemem = freemem;  
 
-    if(copyout(p->pagetable, addr, (char *)&si, sizeof(si)) < 0);
+    if (copyout(p->pagetable, addr, (char *)&si, sizeof(si)) < 0)
       {return -1;}
     return 0;
   }
